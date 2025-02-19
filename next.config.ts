16eb48b46327117d6d665793de649a7ddf110890
Franciszek <franekczajkaa@gmail.com>
2025-02-19 22:59:16 +0100

    Format the app with prettier

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
 
-const withMDX = createMDX({
-});
+const withMDX = createMDX({});
 
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
