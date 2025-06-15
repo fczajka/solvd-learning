d507a8be62690c9f323656913c462eb01983d08a
Franciszek Czajka <franekczajkaa@gmail.com>
2025-06-15 21:41:46 +0200

    Add next lecture from 3rd block

--- public/content.ts
+++ public/content.ts
@@ -33,4 +33,5 @@ export const block_3 = [
   { name: 'React API', href: '/block-three/react-api' },
   { name: 'Next JS, part 1', href: '/block-three/next-js-1' },
   { name: 'Next JS, part 2', href: '/block-three/next-js-2' },
+  { name: 'Next JS, part 3', href: '/block-three/next-js-3' },
 ];
